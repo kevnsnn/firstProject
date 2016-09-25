8e7cd4c576dd1015b7c23525038197eaadb7c9ed
Kevin <kevcubero@gmail.com>
2016-09-25 22:28:46 +0200

    endpoints del API implementados

--- server.js
+++ server.js
@@ -22,6 +22,61 @@ var Todo = mongoose.model('Todo', {
     text: String
 });
 
+// Rutas de nuestro API
+// GET de todos los TODOs
+app.get('/api/todos', function(req, res) {  
+    Todo.find(function(err, todos) {
+        if(err) {
+            res.send(err);
+        }
+        res.json(todos);
+    });
+});
+
+// POST que crea un TODO y devuelve todos tras la creación
+app.post('/api/todos', function(req, res) {  
+    Todo.create({
+        text: req.body.text,
+        done: false
+    }, function(err, todo){
+        if(err) {
+            res.send(err);
+        }
+
+        Todo.find(function(err, todos) {
+            if(err){
+                res.send(err);
+            }
+            res.json(todos);
+        });
+    });
+});
+
+// DELETE un TODO específico y devuelve todos tras borrarlo.
+app.delete('/api/todos/:todo', function(req, res) {  
+    Todo.remove({
+        _id: req.params.todo
+    }, function(err, todo) {
+        if(err){
+            res.send(err);
+        }
+
+        Todo.find(function(err, todos) {
+            if(err){
+                res.send(err);
+            }
+            res.json(todos);
+        });
+
+    })
+});
+
+// Carga una vista HTML simple donde irá nuestra Single App Page
+// Angular Manejará el Frontend
+app.get('*', function(req, res) {  
+    res.sendfile('./public/index.html');                
+});
+
 // Escucha en el puerto 8080 y corre el server
 app.listen(8080, function() {  
     console.log('App listening on port 8080');
